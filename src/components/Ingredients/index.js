d49359aa7f25340d523c2a245d8647e5393d2641
unknown <fredericreact@gmail.com>
2022-09-15 00:24:17 +0800

    ingredients static done

--- src/components/Ingredients/index.js
+++ src/components/Ingredients/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
-
+import './styles.css'
 const Ingredients = () => (
-<div>
-    Ingredients
+<div className='ingredients'>
+<div className='ingredients'>
+    
+<span className='ingredient-quantity'>
+375g
+</span>
+
+<span className='ingredient-name'>
+de farine tamisee
+
+</span>
+
+</div>
+   
 </div>
 
 
